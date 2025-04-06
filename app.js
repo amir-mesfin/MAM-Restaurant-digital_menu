015d3d3e306749b549818e14b80f93adf46077d7
abushe136 <amir.mesfin136@gmail.com>
2025-04-05 23:46:47 -0300

    set-up

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const ejs = require('ejs');
+
+
+const app =  express();
+app.set('view engine','ejs');
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+});
