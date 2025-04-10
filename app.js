6c11157e11e7679d60eb55f32773e995b20464fd
abushe136 <amir.mesfin136@gmail.com>
2025-04-09 23:36:47 -0300

    set up db

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const mainRoutes = require('./routes/main');
 const ejs = require('ejs');
