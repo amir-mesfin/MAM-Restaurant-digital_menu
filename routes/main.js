6a71bcc0d2f9d964f16b89db70f62014f21aa98e
abushe136 <amir.mesfin136@gmail.com>
2025-04-10 20:01:55 -0300

    Add routes for home and menu pages

--- routes/main.js
+++ routes/main.js
@@ -6,38 +6,7 @@ router.get('/', (req, res) => {
   res.render('home');
 });
 
-// Menu page
 router.get('/menu', (req, res) => {
-  // Optional: pass in menu items from database
   res.render('menu');
 });
-
-// Reservations page
-router.get('/reservations', (req, res) => {
-  res.render('reservations');
-});
-
-// About page
-router.get('/about', (req, res) => {
-  res.render('about');
-});
-
-// Contact page
-router.get('/contact', (req, res) => {
-  res.render('contact');
-});
-
-// Order page
-router.get('/order', (req, res) => {
-  res.render('order');
-});
-
-// Login page
-router.get('/login', (req, res) => {
-  res.render('login');
-});
-router.get('/register', (req, res) => {
-  res.render('register');
-});
-
 module.exports = router;
