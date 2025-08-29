307cc9ba0210ec55150d68c162027989cc4af563
abushe136 <amir.mesfin136@gmail.com>
2025-08-29 12:31:19 +0300

    make the heder shaddow

--- client/src/components/Carousels.jsx
+++ client/src/components/Carousels.jsx
@@ -8,7 +8,7 @@ export default function Carousels() {
   // const CarouselImages = [crousel1, crousel2, crousel3, crousel4, crousel5, crousel6, crousel7];
 
   return (
-    <div className="w-full max-w-6xl mx-auto rounded-2xl overflow-hidden shadow-lg">
+    <div className="w-full max-w-full mx-auto rounded-2xl overflow-hidden shadow-lg">
       <Carousel
         autoPlay
         infiniteLoop
@@ -27,7 +27,7 @@ export default function Carousels() {
     />
     {/* Centered text */}
     <div className="absolute inset-0 flex items-center justify-center">
-      <h2 className="text-white text-3xl md:text-5xl font-bold text-center mx-8">
+      <h2 className="text-white text-3xl md:text-5xl font-bold text-center mx-8 talic drop-shadow-[5px_5px_7px_rgba(0,0,0,0.9)]">
         {item.text}
       </h2>
     </div>
