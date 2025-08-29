449ceef805580174e028604c1c77fd76aeeb9bd6
abushe136 <amir.mesfin136@gmail.com>
2025-08-29 10:40:32 +0300

    corecction to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import logo from '../../public/image/mamLogo.png'
+
 export default function Header() {
   return (
-    <div className='flex  items-center  gap-3  justify-center bg-[#774116] '>
-         <img src={logo} alt="" 
-              className='h-23 w-23 rounded-full'/>
-         <h1 className=''>Mam Restaurant </h1>
+    <div className="flex items-center gap-3 justify-center bg-[#784115] p-4 shadow-md">
+      <img 
+        src={logo} 
+        alt="Mam Restaurant Logo" 
+        className="h-16 w-16 rounded-full object-cover"
+      />
+      <h1 className="text-3xl  font-bold text-white tracking-wide italic drop-shadow-[5px_5px_7px_rgba(0,0,0,0.9)]">
+      ማም ሬስቶራንት
+      </h1>
     </div>
   )
 }
