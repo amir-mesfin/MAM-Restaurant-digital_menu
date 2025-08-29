4822f2f2f3cda5c79d2f7fe79998c2dbd4df4b60
abushe136 <amir.mesfin136@gmail.com>
2025-08-29 12:27:46 +0300

    ad crousel image

---
+++ client/src/components/Carousels.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import data from './ctousel.js'
+
+
+export default function Carousels() {
+  // const CarouselImages = [crousel1, crousel2, crousel3, crousel4, crousel5, crousel6, crousel7];
+
+  return (
+    <div className="w-full max-w-6xl mx-auto rounded-2xl overflow-hidden shadow-lg">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+        interval={2000}
+        className="rounded-2xl"
+      >
+        {data.map((item, index) => (
+  <div key={index} className="relative w-full h-[400px] md:h-[500px]">
+    <img
+      src={item.image}
+      alt={`carousel-${index}`}
+      className="w-full h-full object-cover"
+    />
+    {/* Centered text */}
+    <div className="absolute inset-0 flex items-center justify-center">
+      <h2 className="text-white text-3xl md:text-5xl font-bold text-center mx-8">
+        {item.text}
+      </h2>
+    </div>
+  </div>
+))}
+
+      </Carousel>
+    </div>
+  );
+}
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import { Carousel } from 'react-responsive-carousel'
+import Carousels from '../components/Carousels'
 export default function Home() {
   return (
     <div>
       <Header />
+      <Carousels />
     </div>
   )
 }
