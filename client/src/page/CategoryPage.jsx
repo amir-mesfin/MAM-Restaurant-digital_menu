5e2e3a4f0dd2950d719bcda3e115f09287b3b3a9
abushe136 <amir.mesfin136@gmail.com>
2025-09-11 10:41:25 +0300

    worke on add the catagory

---
+++ client/src/page/CategoryPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CategoryPage() {
+  return (
+    <div>
+      <h1 className=' text-white  text-2xl semi-bold '> new  yemgb  mdb lemechemer </h1>
+    </div>
+  )
+}
+http://localhost:5173/   http://localhost:5173/ http://localhost:5173/amir    http://localhost:5173/Amir@
