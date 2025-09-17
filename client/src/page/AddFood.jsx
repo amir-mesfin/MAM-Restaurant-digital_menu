8cff07777a980c03dbe6a5b7ce24b0ca33af7f08
abushe136 <amir.mesfin136@gmail.com>
2025-09-17 03:06:22 +0300

    add default option to category select in AddFood component

--- client/src/page/AddFood.jsx
+++ client/src/page/AddFood.jsx
@@ -21,26 +21,39 @@ export default function AddFood() {
   return (
     <div className=''>
       <h1 className='text-2xl font-semibold max-auto italic text-center mb-7'>Add food</h1>
+
+
       <form className=' max-w-3xl mx-auto flex flex-col space-y-7'>
         <div className='flex flex-col italic font-semibold space-y-1.5 '>
           <label className=''> Food name</label>
-          <input type="text" className='ml-3 p-3 bg-white rounded-lg border border-gray-300  focus:outline-none focus:ring-2 focus:ring-amber-500' placeholder='inter food  name' />
+          <input type="text"
+               className='ml-3 p-3 bg-white rounded-lg border border-gray-300  focus:outline-none focus:ring-2 focus:ring-amber-500' 
+               id='foodName'
+               placeholder='inter food  name' />
         </div>
 
         <div className='flex flex-col italic font-semibold space-y-1.5 '>
           <label className=''> Food description </label>
-          <input type="text" className='ml-3 p-3 bg-white rounded-lg border border-gray-300  focus:outline-none focus:ring-2 focus:ring-amber-500' placeholder='only 12 word ' />
+          <input 
+             type="text" 
+             className='ml-3 p-3 bg-white rounded-lg border border-gray-300  focus:outline-none focus:ring-2 focus:ring-amber-500' 
+             id="foodDescription"
+             placeholder='only 12 word ' />
         </div>
         <div className='flex flex-col italic font-semibold space-y-1.5 '>
           <label className=''>price</label>
-          <input type="number" className='ml-3 p-3 bg-white rounded-lg border border-gray-300  focus:outline-none focus:ring-2 focus:ring-amber-500' placeholder='price food' />
+          <input 
+              type="number"
+              className='ml-3 p-3 bg-white rounded-lg border border-gray-300  focus:outline-none focus:ring-2 focus:ring-amber-500' 
+              id="foodPrice"
+              placeholder='price food' />
         </div>
         <div className='flex flex-col italic font-semibold space-y-1.5 '>
           <label className=''>add food image</label>
           <input
             type="file"
-            // onChange={handleImageChange}
-            className=' ml-3 flex-1 p-2 border border-gray-300 rounded-lg file:mr-3 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-medium file:bg-amber-50 file:text-amber-700 hover:file:bg-amber-100 bg-white'
+               onChange={handleImageChange}
+               className=' ml-3 flex-1 p-2 border border-gray-300 rounded-lg file:mr-3 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-medium file:bg-amber-50 file:text-amber-700 hover:file:bg-amber-100 bg-white'
           />
         </div>
         <div className="flex flex-col italic font-semibold space-y-1.5">
@@ -50,6 +63,7 @@ export default function AddFood() {
             id=""
             className="ml-3 p-3 bg-white rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-amber-500"
           >
+            <option >Select a category</option>
             {getCategory?.map((item)=>(
             <option key={item._id} value={item.catagoryName}>{item.catagoryName}</option>
 
